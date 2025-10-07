2b7db6e13224c4884a67740bf7aba4d89c2ec0d4
Diego Fontes <diego@secunix.com.br>
2025-10-07 17:19:02 -0300

    Ajuste de configuraçao de rota para o search

--- config/urls_search.py
+++ config/urls_search.py
@@ -11,3 +11,6 @@ urlpatterns = [
     # Global alias to satisfy `{% url 'logout' %}` if used without namespace
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
+
+# Custom error handlers
+handler404 = "config.views_errors.handler404"
